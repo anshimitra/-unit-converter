11221922027711e5932fae122fdbdc6700f96ee0
anshimitra <95974826+anshimitra@users.noreply.github.com>
2023-02-28 20:51:09 +0530

    unit

--- src/app/feet/feet.component.ts
+++ src/app/feet/feet.component.ts
@@ -5,19 +5,69 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './feet.component.html',
   styleUrls: ['./feet.component.css']
 })
+
 export class FeetComponent implements OnInit {
 
   constructor() { }
+    toValue:any= 1;
+    fromValue:any = 12;
 
-    feet:any= 1;
-    inch:any = 12;
+    // meter:any= 1;
+    // milimeter:any= 1;
+    // mile:any= 1;
+    // kilometer:any= 1;
+    // yard:any= 1;
+    // centimeter:any= 1;
+
+    toConvert:any = 'feet';
+    fromConvert:any = 'inches';
 
   ngOnInit(): void {
         
   }
 
-  converter(){
-    this.inch = this.feet * 12;
-    console.log(this.inch);
+  converter(toUnit:any, fromUnit:any){
+    console.log("To Unit = ", toUnit, "From Unit = ", fromUnit);
+
+    if(toUnit == 'feet'){
+      if(fromUnit == 'feet'){
+        this.fromValue = this.toValue;
+      }
+      else if(fromUnit == 'inches'){
+        this.fromValue = (this.toValue * 12).toFixed(2);
+      }
+
+      else if(fromUnit == 'miles'){
+        this.fromValue = (this.toValue * 0.000189).toFixed(2);
+      }
+    }
+
+    else if(toUnit == 'inches'){
+      if(fromUnit == 'inches'){
+        this.fromValue = this.toValue;
+        this.toValue = this.fromValue;
+      }
+      else if (fromUnit == 'feet') {
+        this.toValue = (this.fromValue / 12).toFixed(2);
+        this.fromValue = (this.toValue * 12).toFixed(2);
+      }
+      else if (fromUnit == 'miles') {
+        this.toValue = (this.fromValue * 0.000016).toFixed(2);
+        // this.fromValue = (this.toValue / 0.000016).toFixed(2);
+      }
+    }
+    else if(toUnit == 'miles'){
+      if(fromUnit == 'miles'){
+        this.fromValue = this.toValue;
+      }
+      else if (fromUnit == 'feet') {
+        this.toValue = (this.fromValue / 0.000189).toFixed(2);
+        this.fromValue = (this.toValue * 0.000189).toFixed(2);
+      }
+      else if (fromUnit == 'inches') {
+        this.toValue = (this.fromValue / 0.000016).toFixed(2);
+        // this.fromValue = (this.toValue * 0.000016).toFixed(2);
+      }
+    }
   }
 }
